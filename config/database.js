636db76ffad697144b52a3727a8adf198ff86313
anand7337 <yanianand571@gmail.com>
2026-03-27 15:49:39 +0530

    updated-db

--- config/database.js
+++ config/database.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
 const database = async () => {
-    await mongoose.connect(process.env.DATABASE)
-.then((con) => {
+    // await mongoose.connect(process.env.DATABASE)
+
+  await  mongoose.connect(process.env.DATABASE, {
+  serverSelectionTimeoutMS: 10000,  // 10s timeout
+  socketTimeoutMS: 45000,
+  connectTimeoutMS: 10000,
+  retryWrites: true,
+  w: "majority",
+}).then((con) => {
    console.log(`database connect ${con.connection.host}`);
 })
 }
