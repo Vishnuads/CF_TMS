d655948da9e3bca7d53910bbc6d6e0dcacf7eb20
anand7337 <yanianand571@gmail.com>
2026-03-27 13:37:52 +0530

    updated

--- config/database.js
+++ config/database.js
@@ -1,43 +1,13 @@
-// const mongoose = require('mongoose')
-
-// const database = async () => {
-//     await mongoose.connect(process.env.DATABASE)
-// .then((con) => {
-//    console.log(`database connect ${con.connection.host}`);
-// })
-// }
-
-// module.exports = database
-
-
-
-
-
-
-
-
-
-
-
-
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const database = async () => {
-  try {
-    const con = await mongoose.connect(process.env.DATABASE, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000, // 30s wait to find MongoDB server
-      socketTimeoutMS: 45000,           // 45s socket timeout
-      connectTimeoutMS: 30000,          // 30s connection timeout
-    });
-    console.log(`✅ Database connected: ${con.connection.host}`);
-  } catch (err) {
-    console.error("❌ Database connection error:", err.message);
-  }
-};
+    await mongoose.connect(process.env.DATABASE)
+.then((con) => {
+   console.log(`database connect ${con.connection.host}`);
+})
+}
 
-module.exports = database;
+module.exports = database
 
 
 
